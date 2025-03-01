bd41510dc0b9e496c753e492a307720156e034cc
Arunim Chaudhary <arunim2@gmail.com>
2025-03-01 13:51:11 +0530

    OPEN AI SERVICE SUPPORT

--- enterprise/app/services/llm/base_open_ai_service.rb
+++ enterprise/app/services/llm/base_open_ai_service.rb
@@ -1,10 +1,15 @@
 class Llm::BaseOpenAiService
   DEFAULT_MODEL = 'gpt-4o-mini'.freeze
+  DEFAULT_OPENAI_API_BASE_URI = 'https://api.openai.com'.freeze
 
   def initialize
+    # Initialize the OpenAI client with the API key and optional base url
     @client = OpenAI::Client.new(
       access_token: InstallationConfig.find_by!(name: 'CAPTAIN_OPEN_AI_API_KEY').value,
-      log_errors: Rails.env.development?
+      log_errors: Rails.env.development?,
+      uri_base: InstallationConfig.find_by(name: 'CAPTAIN_API_BASE_URI')&.value || DEFAULT_OPENAI_API_BASE_URI,
+      api_type: 'azure',
+      api_version: '2024-02-15-preview'
     )
     setup_model
   rescue StandardError => e
